9bf08a946ec0412a5589251345917479923dc8c4
Mike Lin <mlin@mlin.net>
2014-12-22 00:28:26 -0800

    viral-ngs-assembly-finisher: check assembly quality

--- viral-ngs-assembly-finisher/src/code.sh
+++ viral-ngs-assembly-finisher/src/code.sh
@@ -14,13 +14,22 @@ main() {
     mkdir -p /broad/software/free/Linux/redhat_5_x86_64/pkgs/r_2.15.1/bin
     ln -s "$(which R)" /broad/software/free/Linux/redhat_5_x86_64/pkgs/r_2.15.1/bin/R
 
+    # run V-FAT scripts to orient & merge contigs
     mkdir foo/
     viral-ngs/tools/scripts/vfat/orientContig.pl raw_assembly.fa reference_genome.fa foo/bar
     viral-ngs/tools/scripts/vfat/contigMerger.pl foo/bar_orientedContigs reference_genome.fa -readfq reads.fa -readfq2 reads2.fa -fakequals 30 foo/bar
     ls -tl foo
 
+    # check assembly quality thresholds
+    python viral-ngs/assembly.py filter_short_seqs foo/bar_assembly.fa "$min_length" "$min_unambig" assembly-vfat.fa
+    if ! test -s assembly-vfat.fa; then
+        dx-jobutil-report-error "The assembly failed quality thresholds (length >= ${min_length}, non-N proportion >= ${min_unambig})" AppError
+        exit 1
+    fi
+
+    # upload outputs
     dx-jobutil-add-output assembly --class=file \
-    	$(dx upload foo/bar_assembly.fa --destination "${raw_assembly_prefix}.finished.fasta" --brief)
+    	$(dx upload assembly-vfat.fa --destination "${raw_assembly_prefix}.finished.fasta" --brief)
     dx-jobutil-add-output contigsMap --class=file \
     	$(dx upload foo/bar_contigsMap.pdf --destination "${raw_assembly_prefix}.finished.contigsMap.pdf" --brief)
 }
