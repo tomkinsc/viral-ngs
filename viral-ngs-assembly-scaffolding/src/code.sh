c4f7bb68ddfc19c101ffc4ddf191d4d885cbf2b9
Mike Lin <mlin@mlin.net>
2015-01-25 17:40:30 -0800

    viral-ngs-assembly-scaffolding: use assembly.py order_and_orient

--- viral-ngs-assembly-scaffolding/src/code.sh
+++ viral-ngs-assembly-scaffolding/src/code.sh
@@ -6,26 +6,15 @@ main() {
     pids=()
     dx cat "$resources" | zcat | tar x -C / & pids+=($!)
     dx cat "$novocraft_tarball" | tar zx & pids+=($!)
-    dx download "$trinity_contigs" -o trinity_contigs.fa & pids+=($!)
-    dx download "$reference_genome" -o reference_genome.fa & pids+=($!)
+    dx download "$trinity_contigs" -o trinity_contigs.fasta & pids+=($!)
+    dx download "$reference_genome" -o reference_genome.fasta & pids+=($!)
     dx download "$trinity_reads" -o reads.bam
     for pid in "${pids[@]}"; do wait $pid || exit $?; done
     export NOVOALIGN_PATH=/home/dnanexus/novocraft
 
-    python viral-ngs/read_utils.py bam_to_fastq reads.bam reads.fa reads2.fa
-
-    # symlink muscle and R in the paths hardcoded into contigMerger.pl
-    mkdir -p /seq/annotation/bio_tools/muscle/3.8/
-    ln -s /home/dnanexus/viral-ngs/tools/build/muscle3.8.31_i86linux64 /seq/annotation/bio_tools/muscle/3.8/muscle
-    mkdir -p /broad/software/free/Linux/redhat_5_x86_64/pkgs/r_2.15.1/bin
-    ln -s "$(which R)" /broad/software/free/Linux/redhat_5_x86_64/pkgs/r_2.15.1/bin/R
-
-    # run V-FAT scripts to orient & merge contigs
-    mkdir foo/
-    vfat/orientContig.pl trinity_contigs.fa reference_genome.fa foo/bar
-    vfat/contigMerger.pl foo/bar_orientedContigs reference_genome.fa \
-                         -readfq reads.fa -readfq2 reads2.fa -fakequals 30 foo/bar
-    ls -tl foo
+    # run assembly.py order_and_orient to scaffold the contigs
+    python viral-ngs/assembly.py order_and_orient \
+        trinity_contigs.fasta reference_genome.fasta vfat_scaffold.fasta --inReads reads.bam
 
     if [ -z "$name" ]; then
         name=${trinity_contigs_prefix%%.*}
@@ -34,7 +23,8 @@ main() {
 
     # run assembly.py impute_from_reference to check assembly quality and clean the contigs
     exit_code=0
-    python viral-ngs/assembly.py impute_from_reference foo/bar_assembly.fa reference_genome.fa scaffold.fasta \
+    python viral-ngs/assembly.py impute_from_reference \
+        vfat_scaffold.fasta reference_genome.fasta scaffold.fasta \
         --newName "${name}_scaffold" --replaceLength "$replace_length" \
         --minLength "$min_length" --minUnambig "$min_unambig" \
             2> >(tee impute.stderr.log >&2) || exit_code=$?
@@ -54,9 +44,7 @@ main() {
     dx-jobutil-add-output modified_scaffold --class=file \
         $(dx upload scaffold.fasta --destination "${name}.scaffold.fasta" --brief)
     dx-jobutil-add-output vfat_scaffold --class=file \
-        $(dx upload foo/bar_assembly.fa --destination "${name}.vfat.fasta" --brief)
-    dx-jobutil-add-output contigsMap --class=file \
-        $(dx upload foo/bar_contigsMap.pdf --destination "${name}.contigsMap.pdf" --brief)
+        $(dx upload vfat_scaffold.fasta --destination "${name}.vfat.fasta" --brief)
 }
 
 first_fasta_header() {
