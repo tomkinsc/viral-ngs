52ebe849d9b9d63a3ecafe4ea3df203ef865456c
Mike Lin <mlin@mlin.net>
2015-02-07 14:26:02 -0800

    viral-ngs-human-depletion: add skip and uncompressed fastq

--- viral-ngs-human-depletion/src/code.sh
+++ viral-ngs-human-depletion/src/code.sh
@@ -3,36 +3,44 @@
 main() {
     set -e -x -o pipefail
 
-    dx cat "$resources" | zcat | tar x -C /
-
-    # Receive the input reads as either a BAM file or a pair of .fastq.gz
+    # Receive the input reads as either a BAM file or a pair of FASTQs
     filename=$(dx describe "$file" --name)
     if [[ "$filename" == *.bam ]]; then
         if [ -n "$paired_fastq" ]; then
-            dx-jobutil-report-error "Received both BAM and FASTQ inputs. Provide either one unmapped BAM file or a pair of gzipped FASTQ files." AppError
+            dx-jobutil-report-error "Received both BAM and FASTQ inputs. Provide either one unmapped BAM file or a pair of FASTQ files." AppError
             exit 1
+        fi
+
+        if [ "$skip" == "true" ]; then
+            dx-jobutil-add-output cleaned_reads --class=file "$file"
+            exit 0
         fi
 
         if [ -z "$sample_name" ]; then
             sample_name="$file_prefix"
         fi
 
+        pids=()
+        dx cat "$resources" | zcat | tar x -C / & pids+=($!)
         dx download "$file" -o input.bam
+        for pid in "${pids[@]}"; do wait $pid || exit $?; done
         # TODO: verify BAM is actually unmapped, contains properly paired reads, etc.
-    elif [[ "$filename" == *.fastq.gz ]]; then
+    elif [[ "$filename" == *.fastq.gz || "$filename" == *.fastq ]]; then
         if [ -z "$paired_fastq" ]; then
-            dx-jobutil-report-error "Missing the second gzipped FASTQ containing mate pairs" AppError
+            dx-jobutil-report-error "Missing the second FASTQ file containing mate pairs" AppError
             exit 1
         fi
-        if [[ ! $(dx describe "$paired_fastq" --name) == *.fastq.gz ]]; then
-            dx-jobutil-report-error "The second input file isn't a gzipped FASTQ (*.fastq.gz)" AppError
+        paired_fastq_name=$(dx describe "$paired_fastq" --name)
+        if [[ "$paired_fastq_name" != *.fastq.gz && "$paired_fastq_name" != *.fastq ]]; then
+            dx-jobutil-report-error "The second input file doesn't appear to be a FASTQ file (*.fastq or *.fastq.gz)" AppError
             exit 1
         fi
         
-        # hack SRA FASTQ read names to make them acceptable to Picard FastqToSam
         pids=()
-        dx cat "$file" | zcat | sed -r 's/(@SRR[0-9]+\.[0-9]+)\.1/\1/' | gzip -c > reads.fastq.gz & pids+=($!)
-        dx cat "$paired_fastq" | zcat | sed -r 's/(@SRR[0-9]+\.[0-9]+)\.2/\1/' | gzip -c > reads2.fastq.gz
+        dx cat "$resources" | zcat | tar x -C / & pids+=($!)
+        # hack SRA FASTQ read names to make them acceptable to Picard FastqToSam
+        maybe_dxzcat "$file" | sed -r 's/(@SRR[0-9]+\.[0-9]+)\.1/\1/' | gzip -c > reads.fastq.gz & pids+=($!)
+        maybe_dxzcat "$paired_fastq" | sed -r 's/(@SRR[0-9]+\.[0-9]+)\.2/\1/' | gzip -c > reads2.fastq.gz
         for pid in "${pids[@]}"; do wait $pid || exit $?; done
 
         if [ -z "$sample_name" ]; then
@@ -41,8 +49,14 @@ main() {
         fi
 
         python viral-ngs/read_utils.py fastq_to_bam reads.fastq.gz reads2.fastq.gz input.bam --sampleName "$sample_name"
+
+        if [ "$skip" == "true" ]; then
+            dx-jobutil-add-output cleaned_reads --class=file \
+                $(dx upload --brief --destination ${sample_name}.unmapped.bam input.bam)
+            exit 0
+        fi
     else
-        dx-jobutil-report-error "The input file isn't BAM or gzipped FASTQ" AppError
+        dx-jobutil-report-error "The input file isn't BAM or FASTQ" AppError
         exit 1
     fi
 
@@ -78,12 +92,21 @@ main() {
         --bmtaggerDbs $local_bmtagger_dbs --blastDbs $local_blast_dbs
 
     # upload outputs
-    dx-jobutil-add-output all_reads --class=file \
+    dx-jobutil-add-output intermediates --class=array:file \
         $(dx upload --brief --destination ${sample_name}.raw.bam raw.bam)
-    dx-jobutil-add-output bmtagger_depleted_reads --class=file \
+    dx-jobutil-add-output intermediates --class=array:file \
         $(dx upload --brief --destination ${sample_name}.bmtagger_depleted.bam bmtagger_depleted.bam)
-    dx-jobutil-add-output rmdup_reads --class=file \
+    dx-jobutil-add-output intermediates --class=array:file \
         $(dx upload --brief --destination ${sample_name}.rmdup.bam rmdup.bam)
     dx-jobutil-add-output cleaned_reads --class=file \
         $(dx upload --brief --destination ${sample_name}.cleaned.bam cleaned.bam)
+}
+
+maybe_dxzcat() {
+    name=$(dx describe "$1" --name)
+    if [[ "$name" == *.gz ]]; then
+        dx cat "$1" | zcat
+    else
+        dx cat "$1"
+    fi
 }
