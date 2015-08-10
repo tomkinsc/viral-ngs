41bd09cc5ca3447052f792d2e4f17dd0c59a7cfd
yifei-men <ymen@dnanexus.com>
2015-08-10 11:35:50 -0700

    Use taxon_filter.py utility lasta_build_db for building lastal database

--- viral-ngs-filter/src/viral-ngs-filter.sh
+++ viral-ngs-filter/src/viral-ngs-filter.sh
@@ -12,8 +12,10 @@ main() {
     dx download "$reads" -o reads.bam
     for pid in "${pids[@]}"; do wait $pid || exit $?; done
 
-    # build Lastal target database
-    viral-ngs/tools/build/last-490/bin/lastdb targets.db targets.fasta
+    # build Lastal target database to working dir with prefix targets.db
+    # taxon_filter.py lastal_build_db [input_fasta] [output_dir] [output_prefix]
+    python viral-ngs/taxon_filter.py lastal_build_db targets.fasta ./ targets.db
+    ls
     sha256sum targets.*
 
     # filter the reads
