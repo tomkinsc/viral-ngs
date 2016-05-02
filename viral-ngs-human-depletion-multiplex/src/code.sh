abce474873a5cf18084e0da0cadd80c4f61fd72e
Mike Lin <mlin@mlin.net>
2016-05-01 20:58:31 -0400

    fixup

--- viral-ngs-human-depletion-multiplex/src/code.sh
+++ viral-ngs-human-depletion-multiplex/src/code.sh
@@ -6,7 +6,7 @@ main() {
     depletion_applet_id=$(dx-jobutil-parse-link "$depletion_applet")
     
     for bam in "${bams[@]}"; do
-        job=$(dx run $depletion_applet_id -i "bam=$bam" -i "resources=$resources" -y --brief)
+        job=$(dx run $depletion_applet_id -i "file=$bam" -i "resources=$resources" -y --brief)
         dx-jobutil-add-output --class=array:jobref cleaned_reads $job:cleaned_reads
     done
 }
