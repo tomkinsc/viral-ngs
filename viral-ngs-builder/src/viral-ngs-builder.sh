109216622e32002e35a899b58cea8f52fe223843
Mike Lin <mlin@mlin.net>
2016-10-08 08:45:13 -0700

    Begin docker transition: add dx-docker and viral-ngs docker image to resources tarball

--- viral-ngs-builder/src/viral-ngs-builder.sh
+++ viral-ngs-builder/src/viral-ngs-builder.sh
@@ -30,6 +30,22 @@ main() {
     # record a manifest of the filesystem before doing anything further
     (find / -type f -o -type l 2> /dev/null || true) | sort > /tmp/fs-manifest.0
 
+    # deploy dx-docker beta (TODO: eliminate once dx-docker is stable in production)
+    apt-get -qq install -y python-virtualenv
+    git clone -b dx-docker-beta --recursive https://github.com/dnanexus/dx-toolkit.git dx-docker-beta
+    make -C dx-docker-beta python dx-docker
+    source dx-docker-beta/environment
+
+    # pull the viral-ngs docker image
+    dx-docker pull -q broadinstitute/viral-ngs$viral_ngs_version
+    # generate a script to invoke the viral-ngs docker image
+    echo "dx-docker run -v \$(pwd):/user-data --entrypoint ./env_wrapper.sh broadinstitute/viral-ngs$viral_ngs_version" > /usr/local/bin/viral-ngs
+    chmod +x /usr/local/bin/viral-ngs
+
+
+    # local installation of viral-ngs; this will be made redundant once we finish
+    # docker conversion.
+
     # we need to unset the PYTHONPATH for the conda env
     DX_PYTHON_PATH=$PYTHONPATH
     unset PYTHONPATH
@@ -45,7 +61,7 @@ main() {
 
     # diff the two manifests to get a list of all new files and symlinks
     comm -1 -3 /tmp/fs-manifest.0 /tmp/fs-manifest.1 | \
-      egrep -v "^/proc" | egrep -v "^/sys" | egrep -v "^/tmp" | egrep -v "/\.git/" \
+      egrep -v "^/proc" | egrep -v "^/sys" | egrep -v "/\.git/" \
       > /tmp/resources-manifest.txt
 
     # reset the PYTHONPATH for dx upload to work
