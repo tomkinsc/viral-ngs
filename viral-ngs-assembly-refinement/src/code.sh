ca720758de379e634c15361ea900c54fda1deac3
Mike Lin <mlin@mlin.net>
2016-10-10 20:00:15 -0700

    dockerize refinement

--- viral-ngs-assembly-refinement/src/code.sh
+++ viral-ngs-assembly-refinement/src/code.sh
@@ -15,39 +15,15 @@ main() {
     dx cat "$gatk_tarball" | tar jx -C gatk/
     for pid in "${pids[@]}"; do wait $pid || exit $?; done
 
-    # Stash the PYTHONPATH used by dx
-    DX_PYTHONPATH=$PYTHONPATH
-    DX_PATH=$PATH
+    viral-ngs novoindex /user-data/assembly.nix /user-data/assembly.fasta
 
-    # Load viral-ngs virtual environment
-    # Disable error propagation for now (there are warning :/ )
-    unset PYTHONPATH
+    viral-ngs assembly.py refine_assembly /user-data/assembly.fasta /user-data/reads.bam /user-data/refined_assembly.fasta \
+        --outVcf /user-data/sites.vcf.gz --min_coverage "$min_coverage" --novo_params "$novoalign_options" \
+        --major_cutoff "$major_cutoff" --GATK_PATH /user-data/gatk
+        # TODO: --NOVOALIGN_LICENSE_PATH ?
 
-    set +e +o pipefail
-    export SKIP_VERSION_CHECK=1
-    source easy-deploy-viral-ngs.sh load
-
-    if [ -f /home/dnanexus/novoalign.lic ]; then
-        novoalign-register-license /home/dnanexus/novoalign.lic
-    fi
-
-    gatk-register /home/dnanexus/gatk/GenomeAnalysisTK.jar
-
-    novoindex assembly.nix assembly.fasta
-
-    assembly.py refine_assembly assembly.fasta reads.bam refined_assembly.fasta \
-        --outVcf sites.vcf.gz --min_coverage "$min_coverage" --novo_params "$novoalign_options" \
-        --major_cutoff "$major_cutoff"
-
-    # deactivate viral-ngs virtual environment
-    source deactivate
-
-    # restore paths from DX
-    export PYTHONPATH=$DX_PYTHONPATH
-    export PATH=$DX_PATH
-
-    dx-jobutil-add-output assembly_sites_vcf --class=file \
-        $(zcat sites.vcf.gz | dx upload --destination "${name}.refinement.vcf" --brief -)
-    dx-jobutil-add-output refined_assembly --class=file \
-        $(dx upload refined_assembly.fasta --destination "${name}.refined.fasta" --brief)
+    dxid="$(zcat sites.vcf.gz | dx upload --destination "${name}.refinement.vcf" --brief -)"
+    dx-jobutil-add-output assembly_sites_vcf --class=file "$dxid"
+    dxid="$(dx upload refined_assembly.fasta --destination "${name}.refined.fasta" --brief)"
+    dx-jobutil-add-output refined_assembly --class=file "$dxid"
 }
